87b6e510b871b96a334712f2a3cfd64387c5afcc
Jason Pell <jason@pellcorp.com>
2012-04-12 13:15:22 +1000

    added settings

--- TransactQuotaApk/src/com/pellcorp/android/transact/TransactQuotaActivity.java
+++ TransactQuotaApk/src/com/pellcorp/android/transact/TransactQuotaActivity.java
@@ -1,7 +1,11 @@
 package com.pellcorp.android.transact;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class TransactQuotaActivity extends Activity {
@@ -14,10 +18,28 @@ public class TransactQuotaActivity extends Activity {
         TextView offPeakUsage = (TextView) findViewById(R.id.OffPeakUsage);
         
         peakUsage.setText("25.123");
-        offPeakUsage.setText("25.123");
+        offPeakUsage.setText("26.123");
         
         //TransactQuota quota = new TransactQuota("jpell", "finlay");
         
     }
-   
+    
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, Prefs.class));
+			return true;
+			// More items go here (if any) ...
+		}
+		return false;
+	}
 }
