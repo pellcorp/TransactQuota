0aacf220b421453b278092a503d2631d98a8244f
Jason Pell <jason@pellcorp.com>
2012-04-12 12:20:10 +1000

    added a check to ensure the command line args are provided.

--- TransactQuota/src/test/java/com/pellcorp/android/transact/TransactQuotaRunner.java
+++ TransactQuota/src/test/java/com/pellcorp/android/transact/TransactQuotaRunner.java
@@ -2,10 +2,14 @@ package com.pellcorp.android.transact;
 
 public class TransactQuotaRunner {
 	public static void main(String[] args) {
-		TransactQuota quota = new TransactQuota(args[0], args[1]);
-		Usage usage = quota.getUsage();
-		
-		System.out.println("Peak Usage: " + usage.getPeakUsage());
-		System.out.println("Off Peak Usage: " + usage.getOffPeakUsage());
+		if (args.length == 2) {
+			TransactQuota quota = new TransactQuota(args[0], args[1]);
+			Usage usage = quota.getUsage();
+			
+			System.out.println("Peak Usage: " + usage.getPeakUsage());
+			System.out.println("Off Peak Usage: " + usage.getOffPeakUsage());
+		} else {
+			throw new IllegalArgumentException("Usage: TransactQuotaRunner <username> <password>");
+		}
 	}
 }
