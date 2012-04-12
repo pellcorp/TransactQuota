12b9d47fb3c1a7c979e55c1e4f3c2f14c05ba0e1
Jason Pell <jason@pellcorp.com>
2012-04-12 12:47:11 +1000

    removed obselete dependencies. further work on implementing the initial android ui

--- TransactQuotaApk/src/com/pellcorp/android/transact/TransactQuotaActivity.java
+++ TransactQuotaApk/src/com/pellcorp/android/transact/TransactQuotaActivity.java
@@ -1,22 +1,22 @@
 package com.pellcorp.android.transact;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 public class TransactQuotaActivity extends Activity {
-	private static final String TAG = "Transact Quota";
-	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        TransactQuota quota = new TransactQuota("jpell", "finlay");
+        TextView peakUsage = (TextView) findViewById(R.id.PeakUsage);
+        TextView offPeakUsage = (TextView) findViewById(R.id.OffPeakUsage);
+        
+        peakUsage.setText("25.123");
+        offPeakUsage.setText("25.123");
+        
+        //TransactQuota quota = new TransactQuota("jpell", "finlay");
         
     }
    
