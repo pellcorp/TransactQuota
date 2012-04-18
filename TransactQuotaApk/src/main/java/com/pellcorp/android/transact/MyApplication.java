8a5b9b56c108259cbed9f5d770fb603baafa6913
Jason Pell <jason@pellcorp.com>
2012-04-18 23:29:47 +1000

    minor changes because the NOTIFICATION and dialog are not working for some reason, so fall back to TOAST and automatically send report.

--- TransactQuotaApk/src/main/java/com/pellcorp/android/transact/MyApplication.java
+++ TransactQuotaApk/src/main/java/com/pellcorp/android/transact/MyApplication.java
@@ -1,15 +1,12 @@
 package com.pellcorp.android.transact;
 
 import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "",
-mode = ReportingInteractionMode.TOAST,
-resToastText = R.string.crash_toast_text,
-mailTo = "jason@pellcorp.com") 
+@ReportsCrashes(formKey = "dHVIazZTcEwyblRwQnYzT3I4RWswTkE6MQ",
+	resToastText = R.string.crash_toast_text) 
 public class MyApplication extends Application {
 	@Override
     public void onCreate() {
